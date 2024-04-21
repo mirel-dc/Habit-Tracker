f72f3f7f000c72974eb1025e01070f4bb3386252
Mirel <valerymirel@mail.ru>
2024-04-21 14:16:52 +0500

    deleted debug logs

--- app/src/main/java/com/example/habittracker/fragments/HabitsListFragment.kt
+++ app/src/main/java/com/example/habittracker/fragments/HabitsListFragment.kt
@@ -1,7 +1,6 @@
 package com.example.habittracker.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -82,7 +81,6 @@ class HabitsListFragment : Fragment() {
         }
 
         viewModel.searchNameLiveData.observe(requireActivity()) {
-            Log.d(TAG, "searching $it")
             adapter.submitList(viewModel.getHabitsByType(habitType))
         }
     }
--- app/src/main/java/com/example/habittracker/viewmodels/CreateHabitViewModel.kt
+++ app/src/main/java/com/example/habittracker/viewmodels/CreateHabitViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.habittracker.viewmodels
 
 import android.text.TextUtils
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -54,7 +53,6 @@ class CreateHabitViewModel(
     }
 
     private fun createHabit() = viewModelScope.launch {
-        Log.d(TAG, currentHabit.value.toString())
         currentHabit.value.let {
             if (it != null) {
                 habitRepository.insertHabit(it.copy())
